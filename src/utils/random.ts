2b3df0e475467abb33491cfd3efce7328c47b079
ItzNeviKat <nikitakoschelenko@gmail.com>
2022-02-08 15:52:35 +0300

    fix: not funny random

--- src/utils/random.ts
+++ src/utils/random.ts
@@ -1,22 +1,4 @@
 export function createNodeID(): string {
-  let funny: string[] = [
-    'some',
-    'funny',
-    'and',
-    'unfunny',
-    'words',
-    'cringe',
-    'bebra',
-    'amogus',
-    'vkid',
-    'svgman',
-    'arturfork',
-    'vkterrorists'
-  ];
-
-  return (
-    funny[Math.round(Math.random() * funny.length)] +
-    '-' +
-    Math.random().toString(36).substring(2, 15)
-  );
+  // create random string
+  return Math.random().toString(36).substring(2, 15);
 }
