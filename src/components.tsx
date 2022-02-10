47287ee9b98de20fca351c6e1c11dbccb6b748e6
ItzNeviKat <nikitakoschelenko@gmail.com>
2022-02-10 20:10:00 +0300

    fix: nav or id prop required

--- src/components.tsx
+++ src/components.tsx
@@ -9,29 +9,36 @@ import {
   ModalRoot as VKUIModalRoot,
   ModalRootProps
 } from '@vkontakte/vkui';
+import { NavIdProps, getNavId } from '@vkontakte/vkui/dist/lib/getNavId';
 
 import { useParams } from './hooks';
 import { back } from './router';
-import { getNavId } from '@vkontakte/vkui/dist/lib/getNavId';
+
+// nav or id prop required
+type NavIdRequiredProps = Required<
+  Pick<NavIdProps, 'nav'> | Pick<NavIdProps, 'id'>
+>;
 
 export const View: FC<
-  Omit<ViewProps, 'activePanel' | 'history' | 'onSwipeback'>
+  Omit<ViewProps, 'activePanel' | 'history' | 'onSwipeback'> &
+    NavIdRequiredProps
 > = (props) => (
   // eslint-disable-next-line @typescript-eslint/ban-ts-comment
   // @ts-ignore
   <VKUIView {...props}>{props.children}</VKUIView>
 );
 
-export const Root: FC<Omit<RootProps, 'activeView'>> = (props) => (
+export const Root: FC<Omit<RootProps, 'activeView'> & NavIdRequiredProps> = (
+  props
+) => (
   // eslint-disable-next-line @typescript-eslint/ban-ts-comment
   // @ts-ignore
   <VKUIRoot {...props}>{props.children}</VKUIRoot>
 );
 
-export const Epic: FC<
-  // add nav prop
-  Omit<EpicProps, 'activeStory'> & Pick<RootProps, 'nav'>
-> = (props) => (
+export const Epic: FC<Omit<EpicProps, 'activeStory'> & NavIdRequiredProps> = (
+  props
+) => (
   // eslint-disable-next-line @typescript-eslint/ban-ts-comment
   // @ts-ignore
   <VKUIEpic {...props}>{props.children}</VKUIEpic>
